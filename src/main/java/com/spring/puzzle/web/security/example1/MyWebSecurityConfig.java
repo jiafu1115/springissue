a7106189195b1b8ec7f2c056fa576c79208716f4
jiafu1115 <fujian1115@gmail.com>
2021-06-14 14:05:16 +0800

    fix build issues

--- src/main/java/com/spring/puzzle/web/security/example1/MyWebSecurityConfig.java
+++ src/main/java/com/spring/puzzle/web/security/example1/MyWebSecurityConfig.java
@@ -2,35 +2,47 @@ package com.spring.puzzle.web.security.example1;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Objects;
 
+
+/*
+should uncomment the dependency and related code in pom.xml
+
+<!--        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-security</artifactId>
+        </dependency>
+        -->
+ */
+
+
 @Configuration
-public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new PasswordEncoder() {
-//            @Override
-//            public String encode(CharSequence charSequence) {
-//                return charSequence.toString();
-//            }
-//
-//            @Override
-//            public boolean matches(CharSequence charSequence, String s) {
-//                return Objects.equals(charSequence.toString(), s);
-//            }
-//        };
-//    }
+public class MyWebSecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
+
+/*    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return charSequence.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return Objects.equals(charSequence.toString(), s);
+            }
+        };
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -48,5 +60,5 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin().loginProcessingUrl("/login").permitAll()
                 .and().csrf().disable();
-    }
+    } */
 }
--- src/main/java/com/spring/puzzle/web/security/example2/MyWebSecurityConfig.java
+++ src/main/java/com/spring/puzzle/web/security/example2/MyWebSecurityConfig.java
@@ -2,22 +2,34 @@ package com.spring.puzzle.web.security.example2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Objects;
 
-@Configuration
-public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
+/*
+should uncomment the dependency and related code in pom.xml
+
+<!--        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-security</artifactId>
+        </dependency>
+        -->
+ */
+
+
+@Configuration
+public class MyWebSecurityConfig/* extends WebSecurityConfigurerAdapter*/ {
+
+/*    @Bean
     public PasswordEncoder passwordEncoder() {
         return new PasswordEncoder() {
             @Override
@@ -90,5 +102,5 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin().loginProcessingUrl("/login").permitAll()
                 .and().csrf().disable();
-    }
+    }*/
 }
